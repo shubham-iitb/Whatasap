8ac8e5979db566ed9eefe783645f8d7a61392490
shubhamthebest <sanand.5151@gmail.com>
2018-09-30 21:47:40 +0530

    Search workinghhh

--- lib/newConversation.dart
+++ lib/newConversation.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:whatasap/session.dart';
 import 'package:whatasap/main.dart';
 import 'package:whatasap/chats.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'dart:convert';
 
 class NewConversation extends StatelessWidget {
   // This widget is the root of your application.
@@ -60,84 +62,150 @@ class CreateConvFormState extends State<CreateConvForm> {
   // Note: This is a GlobalKey<FormState>, not a GlobalKey<MyLoginFormState>!
   _ConvData _data = new _ConvData();
   final _formKey = GlobalKey<FormState>();
+  final TextEditingController _typeAheadController = TextEditingController();
+  List dd;
 
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey we created above
 
-    final username = TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      autofocus: false,
-      initialValue: 'p1',
+//    final username = TextFormField(
+//      keyboardType: TextInputType.emailAddress,
+//      autofocus: false,
+//      initialValue: 'p1',
+//      validator: (value) {
+//        if (value.isEmpty) {
+//          return 'Please enter some text';
+//        }
+//      },
+//      onSaved: (String value) {
+//        this._data.userid = value;
+//      },
+//      decoration: InputDecoration(
+//        hintText: 'Username',
+//        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+//        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+//      ),
+//    );
+    final username = TypeAheadFormField(
+
+        textFieldConfiguration: TextFieldConfiguration(
+            controller: this._typeAheadController,
+            decoration: InputDecoration(
+                labelText: 'Name'
+            )
+        ),
+        suggestionsCallback: (pattern) {
+          List<String> list = new List<String>();
+
+          print('ddddddd');
+          Session s = new Session();
+          String urlsend = "http://10.130.154.56:8080/whatsap/AutoCompleteUser";
+          urlsend+= "?term="+pattern.toString();
+          s.get(urlsend).then((response)
+          {
+            print(response);
+            var tt = json.decode(response);
+            print(response);
+
+            dd =tt;
+            print(response);
+          });
+          if(dd!=null) {
+            int length = dd.length;
+            print(length);
+            for (int i = 0; i < length; i++) {
+              if (pattern == ''){
+                print('empty');
+                continue;
+              }
+              print(pattern);
+              if (dd[i]['label'].toString().contains(pattern)) {
+                list.add(dd[i]['label'].toString());
+              }
+            }
+          }
+          print(list);
+            return list;
+            },
+
+        itemBuilder: (context, suggestion) {
+          return ListTile(
+            title: Text(suggestion),
+          );
+        },
+      transitionBuilder: (context, suggestionsBox, controller) {
+        return suggestionsBox;
+      },
+      onSuggestionSelected: (suggestion) {
+        this._typeAheadController.text = suggestion;
+        Session s = new Session();
+        String urlsend = "http://10.130.154.56:8080/whatsap/CreateConversation";
+        print(suggestion.toString());
+        String tokens = suggestion.split("[,]+");
+        print(tokens[1]);
+        print(tokens[0]);
+        urlsend+= "?other_id="+tokens[1].toString();
+        s.get(urlsend).then((response)
+        {
+          print(response);
+          print('----------------------------------------------------------');
+        });
+
+      },
       validator: (value) {
         if (value.isEmpty) {
-          return 'Please enter some text';
+          return 'Please select a city';
         }
       },
-      onSaved: (String value) {
-        this._data.userid = value;
-      },
-      decoration: InputDecoration(
-        hintText: 'Username',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-      ),
     );
 
-    final button = Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        borderRadius: BorderRadius.circular(30.0),
-        shadowColor: Colors.lightBlueAccent.shade100,
-        elevation: 5.0,
-        child: MaterialButton(
-          minWidth: 200.0,
-          height: 42.0,
-          onPressed: () {
-//            Navigator.of(context).pushNamed(HomePage.tag);
-            if (_formKey.currentState.validate()) {
-              // If the form is valid, we want to show a Snackbar
-              _formKey.currentState.save();
+//    final button = Padding(
+//      padding: EdgeInsets.symmetric(vertical: 16.0),
+//      child: Material(
+//        borderRadius: BorderRadius.circular(30.0),
+//        shadowColor: Colors.lightBlueAccent.shade100,
+//        elevation: 5.0,
+//        child: MaterialButton(
+//          minWidth: 200.0,
+//          height: 42.0,
+//          onPressed: () {
+////            Navigator.of(context).pushNamed(HomePage.tag);
+//            if (_formKey.currentState.validate()) {
+//              // If the form is valid, we want to show a Snackbar
+//              _formKey.currentState.save();
+//
+//              print('Printing the login data.');
+//              print('UserID: ${_data.userid }');
+//
+//              var data = new Map();
+////              data['other_id'] = _data.userid;
+//
+//              Session s = new Session();
+//              String urlsend = "http://10.130.154.56:8080/whatsap/CreateConversation";
+//              urlsend+= "?other_id="+_data.userid;
+//              s.get(urlsend).then((response)
+//              {
+//                print(response);
+//                print('----------------------------------------------------------');
+//              });
 
-              print('Printing the login data.');
-              print('UserID: ${_data.userid }');
-
-              var data = new Map();
-//              data['other_id'] = _data.userid;
-
-              Session s = new Session();
-              String urlsend = "http://10.130.154.56:8080/whatsap/CreateConversation";
-              urlsend+= "?other_id="+_data.userid;
-              s.get(urlsend).then((response)
-              {
-                print(response);
-                print('----------------------------------------------------------');
-//                final decodedJSON = json.decode(response);
-//                if(decodedJSON["status"]){
-//                  print('Created new convo');
-//                  Navigator.pushNamed(context, '/chats');
-//                }
-//                else{
-//                  print('Did not create new convo');
-//                }
-              });
-
-              Navigator.push(
-                  context,
-                  new MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                      new Chats()));
-
-
-//              Scaffold.of(context)
-//                  .showSnackBar(SnackBar(content: Text('Processing Data')));
-            }
-          },
-          color: Colors.lightBlueAccent,
-          child: Text('Start Conversation', style: TextStyle(color: Colors.white)),
-        ),
-      ),
-    );
+//              Navigator.push(
+//                  context,
+//                  new MaterialPageRoute(
+//                      builder: (BuildContext context) =>
+//                      new Chats()));
+//
+//
+////              Scaffold.of(context)
+////                  .showSnackBar(SnackBar(content: Text('Processing Data')));
+//            }
+//          },
+//          color: Colors.lightBlueAccent,
+//          child: Text('Start Conversation', style: TextStyle(color: Colors.white)),
+//        ),
+//      ),
+//    );
 
     return new Scaffold(
       appBar: new AppBar(
@@ -189,7 +257,7 @@ class CreateConvFormState extends State<CreateConvForm> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             username,
-            button,
+//            button,
           ],
         ),
       )),
